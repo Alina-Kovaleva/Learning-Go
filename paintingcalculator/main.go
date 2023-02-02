ad20bf54926f68882bc12c85a37cc09e1760f3cd
Alina Kovaleva <calipco.xp@gmail.com>
2023-02-03 00:03:32 +0100

    allow zero values for windows
    also round up number of cans

--- paintingcalculator/main.go
+++ paintingcalculator/main.go
@@ -2,31 +2,30 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"github.com/Alina-Kovaleva/paintingcalculator/utils"
 )
 
 func main() {
 
-	wallLength := utils.UserInput("Enter the combined length of all walls (in meters): ")
-	wallHeight := utils.UserInput("Enter the height of the wall (in meters): ")
-	windowsArea := utils.UserInput("Enter the total area of windows (in square meters): ")
-	doorsArea := utils.UserInput("Enter the total area of doors (in square meters): ")
-	consumption := utils.UserInput("Enter the paint consumption indicated on the paint can (in square meters per liter): ")
-	literPerCan := utils.UserInput("Enter how many liters in one can of paint: ")
-	coats := utils.UserInput("How many of coats of paint you want to apply to the area?: ")
-	pricePerCan := utils.UserInput("Enter the price for one can of paint: ")
+	wallLength := utils.UserInput("Enter the combined length of all walls (in meters): ", false)
+	wallHeight := utils.UserInput("Enter the height of the wall (in meters): ", false)
+	windowsArea := utils.UserInput("Enter the total area of windows (in square meters): ", true)
+	doorsArea := utils.UserInput("Enter the total area of doors (in square meters): ", false)
+	consumption := utils.UserInput("Enter the paint consumption indicated on the paint can (in square meters per liter): ", false)
+	literPerCan := utils.UserInput("Enter how many liters in one can of paint: ", false)
+	coats := utils.UserInput("How many of coats of paint you want to apply to the area?: ", false)
+	pricePerCan := utils.UserInput("Enter the price for one can of paint: ", false)
 
 	totalArea := wallLength * wallHeight
 	paintedArea := totalArea - windowsArea - doorsArea
 
 	coverinPerCan := consumption * literPerCan
 
-	totalCans := (paintedArea / coverinPerCan) * coats
+	totalCans := math.Ceil((paintedArea / coverinPerCan) * coats)
 
 	totalCost := totalCans * pricePerCan
 
-	fmt.Printf("You will need %.2f cans of paint and it will cost %.2f", totalCans, totalCost)
+	fmt.Printf("You will need %.0f cans of paint and it will cost %.2f", totalCans, totalCost)
 
 }
-
-
--- paintingcalculator/utils/prompt.go
+++ paintingcalculator/utils/prompt.go
@@ -7,14 +7,16 @@ import (
 	"strconv"
 )
 
-func UserInput(question string) float64 {
+func UserInput(question string, allowZero bool) float64 {
 	for {
 		scanner := bufio.NewScanner(os.Stdin)
 		fmt.Print(question)
 		scanner.Scan()
 		answer, err := strconv.ParseFloat(scanner.Text(), 64)
-		if err != nil || answer <= 0 {
+		if err != nil {
 			fmt.Println("Please enter valid value")
+		} else if answer < 0 || (answer == 0 && !allowZero) {
+			fmt.Println("Please enter positive value")
 		} else {
 			return answer
 		}
